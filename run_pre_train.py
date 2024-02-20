bd26b2fa2bde729bf3b2522b335b03cf91935c49
Emanuele Pietropaolo <edoppiap@gmail.com>
2024-02-21 00:12:35 +0100

    Small fix

--- run_pre_train.py
+++ run_pre_train.py
@@ -101,8 +101,8 @@ def eval(args, test_dataset, model, output_folder):
                 nsp_preds = nsp_logits.detach().cpu()
                 nsp_truths = next_sentence_label.detach().cpu()
             else:
-                nsp_preds = torch.concat((nsp_preds, nsp_logits.detach().cpu()), dim=0)
-                nsp_truths = torch.concat((nsp_truths, next_sentence_label.detach().cpu()), dim=0)
+                nsp_preds = torch.cat((nsp_preds, nsp_logits.detach().cpu()), dim=0)
+                nsp_truths = torch.cat((nsp_truths, next_sentence_label.detach().cpu()), dim=0)
         else:
             outputs = model(input_ids=input_ids,
                     token_type_ids = token_type_ids,
