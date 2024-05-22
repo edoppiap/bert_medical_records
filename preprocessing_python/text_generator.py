201c1d30d1f27fb9b25e308b69eaf0421c37d5a7
Emanuele Pietropaolo <edoppiap@gmail.com>
2024-05-22 11:14:26 +0200

    New data format 2

--- preprocessing_python/text_generator.py
+++ preprocessing_python/text_generator.py
@@ -7,18 +7,30 @@ import os
 from tqdm import tqdm
 from stqdm import stqdm #Streamlit-compatible progress bar
 import streamlit as st
-from datetime import datetime, timedelta
+from datetime import datetime
 import argparse
 import random
 
-def create__infer_from_data(dataframe_or_file_path, output_folder, output_name = 'infer_dataset.txt', streamlit=False):
+def create_infer_from_data(dataframe_or_file_path, output_folder, output_name = 'infer_dataset.txt', streamlit=False):
+    """Function that generate the dataset for the Prediction out of the input csv file. It is needed only for development
+    reason.
+
+    Args:
+        dataframe_or_file_path (_type_): DataFrame object or a path to the input csv file
+        output_folder (_type_): Folder where to save the output text file
+        output_name (str, optional): Output file name. Defaults to 'infer_dataset.txt'.
+        streamlit (bool, optional): _description_. Defaults to False.
+
+    Returns:
+        _type_: The path in which has been save the text dataset
+    """
     #output_path = os.path.join(dataframe_or_folder, text_generated_name)
     if isinstance(dataframe_or_file_path, pd.DataFrame): # it means that there is directly the df file
-        grouped_df = dataframe_or_file_path.groupby('keyone')
+        grouped_df = dataframe_or_file_path.groupby('patientID')
         
     elif os.path.exists(dataframe_or_file_path):
         df = pd.read_csv(dataframe_or_file_path, index_col=0)
-        grouped_df = df.groupby('keyone')
+        grouped_df = df.groupby('patientID')
     
     progress_text = 'Producing text file from csv dataset'
     if streamlit:
@@ -27,9 +39,9 @@ def create__infer_from_data(dataframe_or_file_path, output_folder, output_name =
         loop = tqdm(grouped_df, desc=progress_text)
 
     results = []
-    for keyone, patient in loop:
-        result = f'{str(keyone)}, [CLS] '
-        #result = str(keyone) + '\n'
+    for patientID, patient in loop:
+        result = f'{str(patientID)}, [CLS] '
+        #result = str(patientID) + '\n'
         for _, row in patient.iterrows():
             result = result + f"{row['main_ICD9']}"
             if not pd.isna(row['ICD9_1']):
@@ -56,10 +68,18 @@ def create__infer_from_data(dataframe_or_file_path, output_folder, output_name =
         
     return text_dataset_path
 
-def create_finetune_text_from_data(output_folder, file_path='/content/drive/MyDrive/EHR/base_red3.csv', output_name = 'finetune_dataset.txt'):
+def create_finetune_text_from_data(output_folder, file_path='data\PHeP_simulated_data.csv', output_name = 'finetune_dataset.txt'):
+    """Function that generate the finetuning dataset, it delete the last hospitalization event and label the remaining events with 1 if the 
+    deleted event is earlier than 90 days, 0 otherwise
+
+    Args:
+        output_folder (_type_): Folder in which save the text_dataset
+        file_path (str, optional): _description_. Defaults to 'data\PHeP_simulated_data.csv'.
+        output_name (str, optional): _description_. Defaults to 'finetune_dataset.txt'.
+    """
     df = pd.read_csv(file_path, index_col=0)
     
-    grouped_df = df.groupby('keyone')
+    grouped_df = df.groupby('patientID')
 
     all_diagnoses = []
     all_hospitalisations = []
@@ -151,11 +171,11 @@ def create_nsp_dataset(file_path, output_folder, output_name='nsp_dataset.txt'):
 def create_text_from_data(dataframe_or_file_path, output_folder, output_name = 'text_dataset.txt', streamlit=False):
     #output_path = os.path.join(dataframe_or_folder, text_generated_name)
     if isinstance(dataframe_or_file_path, pd.DataFrame): # it means that there is directly the df file
-        grouped_df = dataframe_or_file_path.groupby('keyone')
+        grouped_df = dataframe_or_file_path.groupby('patientID')
         
     elif os.path.exists(dataframe_or_file_path):
         df = pd.read_csv(dataframe_or_file_path, index_col=0)
-        grouped_df = df.groupby('keyone')
+        grouped_df = df.groupby('patientID')
     
     progress_text = 'Producing text file from csv dataset'
     if streamlit:
@@ -166,7 +186,7 @@ def create_text_from_data(dataframe_or_file_path, output_folder, output_name = '
     results = []
     for i, (_, patient) in enumerate(loop):
         result = '[CLS] '
-        #result = str(keyone) + '\n'
+        #result = str(patientID) + '\n'
         for _, row in patient.iterrows():
             result = result + f"{row['main_ICD9']}"
             if not pd.isna(row['ICD9_1']):
@@ -199,7 +219,7 @@ if __name__ == '__main__':
     
     parser.add_argument('--file_path', type=str,
                         help='Folder where are located the input csv file')
-    parser.add_argument('--output_name', type=str, default='dataset_text.txt',
+    parser.add_argument('--output_name', type=str, default='dataset.txt',
                         help='Name for the output text file')
     parser.add_argument('--output_folder', type=str,
                         help='Folder where to save the output text file')
@@ -219,7 +239,7 @@ if __name__ == '__main__':
                                        file_path=args.file_path,
                                        output_name=args.output_name)
     if args.create_infer_text_data:
-        create__infer_from_data(args.file_path, 
+        create_infer_from_data(args.file_path, 
                               output_folder=args.output_folder, 
                               output_name=args.output_name)
         
