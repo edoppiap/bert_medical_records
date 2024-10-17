c06ea45807c309aba99b14d73ec7e8ae6319a0a9
Emanuele Pietropaolo <edoppiap@gmail.com>
2024-10-17 11:59:45 +0200

    Fix log message variable initialization

--- run_pre_train.py
+++ run_pre_train.py
@@ -314,12 +314,12 @@ def main():
             +'(They can vary a little between split)')
     
     if args.do_train:
+        start_time = datetime.now()
         if args.k_fold ==1:
             model_output_path = os.path.join(output_path, 'pre_trained_model')
             loss = train(args,train_dataset,model,model_output_path)
         else:
             loss = 0
-            start_time = datetime.now()
             for i,X in enumerate(train_dataset):
                 model_output_path = os.path.join(output_path, f'model_{i}', 'pre_trained_model')
                 loss += train(args, X, models[i], model_output_path)
